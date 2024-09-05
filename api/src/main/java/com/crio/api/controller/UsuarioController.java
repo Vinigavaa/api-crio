77c492093108e5e5a582f3dd4d975ba3d94b2eb7
Vinigavaa <vinigava14@gmail.com>
2024-09-04 21:27:01 -0300

    feat:make UsuarioController.

---
+++ api/src/main/java/com/crio/api/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.crio.api.controller;
+
+import com.crio.api.domain.usuario.Usuario;
+import com.crio.api.domain.usuario.UsuarioRequestDTO;
+import com.crio.api.repositorie.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping(consumes = "multpart/form-data")
+    public ResponseEntity<Usuario> create (
+            @RequestParam("nomecompleto") String nomeCompleto,
+            @RequestParam("email") String email,
+            @RequestParam("senha") String senha,
+            @RequestParam("tipo") int tipo){
+        UsuarioRequestDTO usuarioRequestDTO = new UsuarioRequestDTO(nomeCompleto, email, senha, tipo);
+        Usuario newUsuario = this.usuarioService.createUsuario(usuarioRequestDTO);
+        return ResponseEntity.ok(newUsuario);
+    }
+}
